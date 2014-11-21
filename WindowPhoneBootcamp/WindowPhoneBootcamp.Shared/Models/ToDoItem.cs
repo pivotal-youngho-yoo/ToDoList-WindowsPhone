710c6662ec7f23dceedb6ce93b5c647b3b3fe908
xtreme-brandon-sproul <bsproul@pivotal.io>
2014-11-21 14:34:30 -0600

    initial commit. Created a ToDoList model and view model. Need to bind the data to a view.

---
+++ WindowPhoneBootcamp/WindowPhoneBootcamp.Shared/Models/ToDoItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowPhoneBootcamp.Models
+{
+    class ToDoItem
+    {
+        private string task;
+        private bool completed;
+
+        public ToDoItem(string task, bool completed)
+        {
+            this.task = task;
+            this.completed = completed;
+        }
+
+        public bool Completed
+        {
+            get { return completed; }
+            set { completed = value; }
+        }
+
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+    }
+}
---
+++ WindowPhoneBootcamp/WindowPhoneBootcamp.Shared/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WindowPhoneBootcamp.Models;
+
+namespace WindowPhoneBootcamp.ViewModels
+{
+    class ToDoListViewModel
+    {
+        private List<ToDoItem> toDoList;
+
+        public ToDoListViewModel()
+        {
+            this.toDoList = new List<ToDoItem>();
+        }
+
+        public void AddToDoItem(string task)
+        {
+            ToDoItem newTask = new ToDoItem(task, false);
+            toDoList.Add(newTask);
+        }
+
+        public List<ToDoItem> ToDoList
+        {
+            get { return toDoList; }
+        }
+    }
+}
